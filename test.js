8165297706c102fc620fd2c8e1362f9b97f8d8ad
why-not-try-calmer <mrnycticorax@gmail.com>
2024-06-05 12:48:41 +0200

    ---

--- test.js
+++
@@ -1,8 +0,0 @@
-var m_file = java.io.File("README.md");
-var m_reader = java.io.FileReader(m_file);
-var m_buffer = java.io.BufferedReader(m_reader);
-var found = 0;
-while ((line = m_buffer.readLine()) != null) {
-    found += 1
-}
-java.lang.System.out.println(found == 164);
